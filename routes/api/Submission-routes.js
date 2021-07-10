ea824598e7862a958d4c9a27d8d9bb566d9e58c6
Charlie Parrott <cparrott5700@gmail.com>
2021-07-10 11:35:26 -0400

    Adding routes pages

---
+++ routes/api/Submission-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+// Get all submissions
+
+module.exports = router;
---
+++ routes/api/User-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+// Get all 
+
+module.exports = router;
