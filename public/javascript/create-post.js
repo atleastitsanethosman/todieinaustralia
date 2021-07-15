674575978ed931ef4892ae7bd106bd8b6b314588
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-07-14 21:19:28 -0400

    significant repair and restructure work for controller files and handlebar templates

--- public/javascript/create-post.js
+++ public/javascript/create-post.js
@@ -4,11 +4,13 @@ async function newFormHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value;
   const post_content = document.querySelector('input[name="post-content"]').value;
 
-  const response = await fetch(`/api/posts`, {
+  const response = await fetch(`/api/Submission-routes`, {
     method: 'POST',
     body: JSON.stringify({
       title,
-      post_content
+      category,
+      text,
+      img_link
     }),
     headers: {
       'Content-Type': 'application/json'
--- public/javascript/edit-submission.js
+++ public/javascript/edit-submission.js
@@ -1,17 +1,17 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_content = document.querySelector('input[name="post-content"]').value;
+  const title = document.querySelector('input[name="submission-title"]').value;
+  const text = document.querySelector('input[name="submission-content"]').value;
   const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
 
-  const response = await fetch(`/api/posts/${id}`, {
+  const response = await fetch(`/api/Submission-routes/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
           title,
-          post_content
+          text
       }),
       headers: {
           'Content-Type': 'application/json'
@@ -25,4 +25,4 @@ async function editFormHandler(event) {
     }
 }
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document.querySelector('.edit-submission-form').addEventListener('submit', editFormHandler);
