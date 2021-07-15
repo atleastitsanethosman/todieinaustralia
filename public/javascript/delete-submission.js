93ed6529934658f16af3fe1a14b869871dd663a6
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-07-15 11:34:48 -0400

    basic CRUD functions working

--- public/javascript/delete-submission.js
+++ public/javascript/delete-submission.js
@@ -5,10 +5,10 @@ async function deleteFormHandler(event) {
       window.location.toString().split('/').length - 1
     ];
 
-  const response = await fetch(`/api/posts/${id}`, {
+  const response = await fetch(`/api/Submission-routes/${id}`, {
       method: 'DELETE',
       body: JSON.stringify({
-        post_id: id
+        submission_id: id
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -23,4 +23,4 @@ async function deleteFormHandler(event) {
   
 }
 
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document.querySelector('.delete-submission-btn').addEventListener('click', deleteFormHandler);
