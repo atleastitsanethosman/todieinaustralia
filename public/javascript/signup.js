169e62ce1a5871dcac8f1ba9661b8bf28f669ecc
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-07-17 12:20:05 -0400

    fixing javascript bug in login file

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -4,8 +4,6 @@ async function signupFormHandler(event) {
   const name = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-  const twitter = document.querySelector('#twitter-signup').value.trim();
-  const github = document.querySelector('#github-signup').value.trim();
 
   if (name && email && password) {
     const response = await fetch('/api/User-routes', {
